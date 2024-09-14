a6c05e379829f64c45389f581f770e63e9f7af8e
difasaltscm <difa.ardiansyah@salt.co.id>
2024-09-14 21:37:26 +0700

    feat: [TSR-000] Session Serve Mux  Go Web

--- handler_test.go
+++ handler_test.go
@@ -21,3 +21,30 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello Word")
+	})
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "images")
+	})
+
+	mux.HandleFunc("/images/thumbnails", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "images thumbnails")
+	})
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
