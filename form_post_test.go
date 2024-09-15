4913a4663dabdedf9c558386211cee5f50c2a883
difasaltscm <difa.ardiansyah@salt.co.id>
2024-09-15 12:11:30 +0700

    feat: [TSR-000] Session FormPost Test Go Web

---
+++ form_post_test.go
@@ -0,0 +1,34 @@
+package go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+	fmt.Fprintf(writer, "Halo nama first %s last %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=Difa&last_name=Ardiansyah")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+
+}
